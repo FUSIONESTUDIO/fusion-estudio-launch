9d4e2ff6f9a21ba12aef311077ce3dc53a0259d2
FUSIONESTUDIO <lustofabrizio2@gmail.com>
2024-12-07 11:55:21 -0300

    Add files via upload

---
+++ fusion-estudio-launch/script.js
@@ -0,0 +1,6 @@
+window.onload = function() {
+  const textElement = document.getElementById('text');
+  setTimeout(function() {
+    textElement.textContent = "11/12/20--4"; // Texto que aparecerá después de la animación.
+  }, 2000); // Aparece después de 2 segundos.
+}
